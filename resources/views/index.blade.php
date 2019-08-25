e11ef7cfa5ccc14070381fdb53f9e10523b258a6
pushap25 <sainipushapal@gmail.com>
2019-08-25 16:36:43 +0530

    updated layouts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,22 +7,70 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<!-- Bootstrap -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 
 	<!-- jQuery Script -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
 	<!-- LinkedIn Profile Link -->
 	<script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+
 	<link rel="stylesheet" href="{{asset('front/css/index.css')}}">
 	<link rel="stylesheet" href="{{asset('front/css/contact.css')}}">
 	<link rel="stylesheet" href="{{asset('front/css/responsive.css')}}">
 
 </head>
 <body>
+	<nav class='navbar navbar-inverse'>
+		<div class="container-fluid">
+			<div class="navbar-header col-md-6">
+				<a class="navbar-brand inline-grid p-t-10 header" href="{{asset('/')}}">
+					<strong>Pushap Saini</strong>
+					<span class="f-10">An Interactive Developer With Passion Of Creativity</span>
+				</a>
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav nav-list">
+					<li><a class='header' href="{{asset('/')}}">Home</a></li>
+					<li>
+						<a class='header' href='#work'>Work</a>
+					</li>
+					<li>
+						<a class='header' href='#projects'>Projects</a>
+					</li>
+					<li>
+						<a class='header' href='#academics'>Academics</a>
+					</li>
+					<li>
+						<a class='header' href='#skills'>Skills</a>
+					</li>
+					<li>
+						<a class='header' href='#contact'>Contact</a>
+					</li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right nav-list">
+					@if(\Auth::check())
+						<li>
+							<a class="header" href="{{asset('admin')}}">Admin</a>
+						</li>
+					@else
+						<li>
+							<a class="header" href="{{route('login')}}">Login</a>
+						</li>
+					@endif
+					<!-- <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li> -->
+				</ul>
+			</div>
+		</div>
+	</nav>
 	<div class="container-fluid">
-		<div class='row'>
+<!-- 		<div class='row'>
 			<div class="navbar-header col-md-6">
 				<a class="navbar-brand inline-grid p-t-10" href="{{asset('/')}}">
 					<strong>Pushap Saini</strong>
@@ -59,15 +107,15 @@
 				@endif
 			</ul>
 		</div>
-		<div class="row img-container">
+ -->		<div class="row img-container">
 			<div class="front-bg">
 				<div class="rotater">
-<!-- 					<div id="home" class="hid">@include('layouts.home')</div>
+ 					<div id="home" class="hid">@include('layouts.home')</div>
 					<div id="work" class="hid">@include('layouts.work')</div>
 					<div id="projects" class="hid">@include('layouts.projects')</div>
 					<div id="academics" class="hid">@include('layouts.academics')</div>
 					<div id="skills" class="hid">@include('layouts.skills')</div>
- -->					<div id="contact" class="contact hid">@include('layouts.contact')</div>
+ 					<div id="contact" class="contact hid">@include('layouts.contact')</div>
 				</div>
 			</div>
 			<!-- <img class="front-bg" src="{{asset('images/edu44.jpg')}}"> -->
